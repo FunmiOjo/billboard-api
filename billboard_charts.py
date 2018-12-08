bba02c00ce5bab6b3122fccc528b241a6144b150
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2018-12-08 11:28:29 -0500

    Added flask

---
+++ billboard_charts.py
@@ -0,0 +1,6 @@
+import billboard
+chart = billboard.ChartData('hot-100')
+rb_chart = billboard.ChartData('r-b-hip-hop-songs', '1969-11-27')
+song = chart[0]
+rb_song = rb_chart[25]
+print(song, rb_song)
